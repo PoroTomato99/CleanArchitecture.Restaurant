dfb0431e09fa22ddf801a6b235b6770f48bae6dd
porotomato <porotomato@gmail.com>
2021-05-31 21:14:40 +0800

    show-restaurant typescript

--- Restaurant.UI.Angular/angular10/src/app/restaurant/show-restaurant/show-restaurant.component.ts
+++ Restaurant.UI.Angular/angular10/src/app/restaurant/show-restaurant/show-restaurant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, IterableDiffers, OnInit } from '@angular/core';
 import {SharedService} from 'src/app/shared.service';
 @Component({
   selector: 'app-show-restaurant',
@@ -9,14 +9,28 @@ export class ShowRestaurantComponent implements OnInit {
 
   constructor(private service:SharedService) { }
 
-  RestaurantList: any = [];
+  RestaurantList : any = [];
   ngOnInit(): void {
     this.refreshRestaurantList()
   }
 
   refreshRestaurantList(){
-    this.service.getRestaurantList().subscribe(data => {
-      this.RestaurantList = data;
-    });
+    this.service.getRestaurantList().subscribe(
+      (Response) => {
+        this.RestaurantList = Response;
+
+        for(let val in this.RestaurantList){
+          for(let val2 in this.RestaurantList[val]){
+            for(let val3 in this.RestaurantList[val][val2]){
+              let openTime = this.RestaurantList[val][val2][val3];
+              if(val3 =='OpenHour'){
+                //console.log(openTime);
+                
+              }
+            }
+          }
+        }
+      }
+    );
   }
 }
